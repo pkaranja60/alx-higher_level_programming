8d5a601156fdee5978b4d2f2ed5876404a267812
Peter karanja <peterkaranja60@gmail.com>
2023-08-17 13:29:21 +0300

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- List shows without corresponding genre_ids
-SELECT tv_shows.title, NULL as genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title ASC;
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
