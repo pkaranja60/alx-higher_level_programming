1da2c81fcf8c1c3bbb4b06a4862d1081a698c26a
Peter karanja <peterkaranja60@gmail.com>
2023-08-17 13:33:57 +0300

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,6 @@
--- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
 -- List all shows and linked genres
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
+SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre_name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title ASC, genre_name ASC;
