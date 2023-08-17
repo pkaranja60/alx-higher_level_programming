b50f19005c6679a8fa235c5be4299f431da122c0
Peter karanja <peterkaranja60@gmail.com>
2023-08-17 13:20:04 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 --list all privileges of the MYSQL users user_0d_1 and user_0d_2
-mysql -u root -p -e "SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
